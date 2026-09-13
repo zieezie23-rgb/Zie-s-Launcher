62b021988f2ac8562b09e06ef33ee08778b62761
zie <zieezie23@gmail.com>
2026-09-13 17:17:31 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="zie-pwa-v11";
-const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./assets/icon.png","./assets/icon-maskable.png"];
+const CACHE="zie-pwa-v12";
+const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./assets/icon.png?v=2","./assets/icon-maskable.png?v=2"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(
  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
